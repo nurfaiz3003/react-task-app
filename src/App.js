27c3d0241b34b741b072e60dae6d603f82af2d7c
Nurfaiz Herlambang <nurfaiz3003@gmail.com>
2021-04-08 19:30:22 +0700

    Add Remainder and Delete Function

--- src/App.js
+++ src/App.js
@@ -9,27 +9,37 @@ function App() {
             id: 1,
             text: 'Go To Supermarket',
             day: 'April 9th at 1.30pm',
-            remider: true 
+            remider: true, 
         },
         {
             id: 2,
             text: 'Go To Doctor',
             day: 'April 9th at 4.30pm',
-            remider: true 
+            remider: true, 
         },
         {
             id: 3,
             text: 'Go To Cipanas',
             day: 'April 9th at 6.30pm',
-            remider: false 
+            remider: false, 
         }
     ]
-);
+  );
+  
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task));
+  }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks') }
     </div>
   );
 }
